c5ba2c01a9aa5d19aab9a1c0c7d3ea3f1e08f6c1
Reem-Nosbaum <reemnu@gmail.com>
2023-06-11 13:13:51 +0300

    :tada: Begin a project.

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textColor}>Hello, world!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textColor: {
+    color: 'red',
+  },
+});
+
+export default App;
